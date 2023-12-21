62fdd25c3f78fd254658a7a52edcd556e54054ca
bobur <boburmakhmadkulov05@gmail.com>
2023-12-21 15:51:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let btn_submit = document.querySelector('.submit')
+let necessary_rows = document.querySelectorAll('.personality input')
+
+necessary_rows.forEach((row) => {
+    if (row.value.length === 0) {
+        row.style.border = "2px solid red"
+        btn_submit.setAttribute('type', 'button')
+    } else if (row.value.length > 0 ) {
+        row.style.border = "2px solid green"
+        btn_submit.setAttribute('type', 'submit')
+    }
+})
