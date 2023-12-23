b1f3bac446bd1ca5f298253629cf54cd988d0f65
bobur <boburmakhmadkulov05@gmail.com>
2023-12-23 12:27:31 +0500

    changes everywhere

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-let btn_submit = document.querySelector('.submit')
-let necessary_rows = document.querySelectorAll('.personality input')
+let form = document.forms.login
+let inp_reqs = form.querySelectorAll('.required input')
+let success = document.querySelector('.success')
+let error = document.querySelector('.error')
 
-btn_submit.onclick = () => {
-    necessary_rows.forEach((row) => {
-        if (row.value.length === 0) {
-            row.style.border = "2px solid red"
-            btn_submit.setAttribute('type', 'button')
-        } else if (row.value.length > 0 ) {
-            row.style.border = "2px solid green"
-            btn_submit.setAttribute('type', 'submit')
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    let error_count = 0
+    let success_count = 7
+
+    inp_reqs.forEach(inp => {
+        if (inp.value.length === 0) {
+            inp.parentElement.classList.add('error-field')
+            error_count++
+            error.innerHTML = "Error: " + error_count + "/7"
+            let answer = success_count - error_count
+            success.innerHTML = "Success: " + answer + "/7"
+        } else {
+            inp.parentElement.classList.remove('error-field')
         }
     })
+
+    if (error_count > 0) {
+        alert('Error!')
+    } else {
+        submit()   
+    }
+}
+
+function submit() {
+    let user = {}
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        user[key] = value
+    })
+
+    console.log(user);
 }
