1b6f8a825d2894b3ea48eba084a0d2414f908ed3
bobur <boburmakhmadkulov05@gmail.com>
2024-01-06 16:04:03 +0500

    second commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ form.onsubmit = (event) => {
     if (error_count > 0) {
         alert('Error!')
     } else {
-        submit()   
+        submit()
     }
 }
 
@@ -38,4 +38,53 @@ function submit() {
     })
 
     console.log(user);
+}
+
+
+
+let names = document.querySelectorAll('.names')
+names.forEach((name) => {
+    name.onkeyup = () => {
+        if (/^[a-z ,.'-]+$/i.test(name.value) || name.value.length === 0) {
+            name.parentElement.classList.remove('error-field')
+        } else {
+            name.parentElement.classList.add('error-field')
+        }
+    }
+});
+
+let y_name = document.querySelector('#name')
+y_name.onkeyup = () => {
+    if (/^[a-z ,.'-]+$/i.test(y_name.value) || y_name.value.length === 0) {
+        y_name.parentElement.classList.remove('error-field')
+    } else {
+        y_name.parentElement.classList.add('error-field')
+    }
+}
+
+let email = document.querySelector('#email')
+email.onkeyup = () => {
+    if (/^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/.test(email.value) || email.value.length === 0) {
+        email.parentElement.classList.remove('error-field')
+    } else {
+        email.parentElement.classList.add('error-field')
+    }
+}
+
+let phone = document.querySelector('#tel')
+phone.onkeyup = () => {
+    if (/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(phone.value) || phone.value.length === 0) {
+        phone.parentElement.classList.remove('error-field')
+    } else {
+        phone.parentElement.classList.add('error-field')
+    }
+}
+
+let age = document.querySelector('#age')
+age.onkeyup = () => {
+    if (/^\S[0-9]{0,1}$/.test(age.value) || age.value.length === 0) {
+        age.parentElement.classList.remove('error-field')
+    } else {
+        age.parentElement.classList.add('error-field')
+    }
 }
